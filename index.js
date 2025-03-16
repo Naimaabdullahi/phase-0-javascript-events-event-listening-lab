8f5eeca5af938fd5e378a0af9e6f0bb77a3b4583
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-16 21:05:20 +0300

    events

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 function addingEventListener() {
-    const button = document.getElementById("button");
-  
-    if (button) {
-      button.addEventListener("click", function () {
-        alert("Button clicked!");
-      });
-    }
+  const button = document.getElementById('button')
+
+  if(button) {
+    button.addEventListener('click',function() {
+      alert("i was clicked")
+    });
   }
+}
+console.log(addingEventListener)
